cfbe5d591ce042dd174a4e86c434f2537676f1fa
Olga Sova <sova_277@mail.ru>
2026-03-11 21:38:37 +0400

    добавлен pom.xml добавлена зависимость для SLF4J: pom.xml добавлен logger в класс Wordle.java добавлен метод loadDictionary(String filePath) в класс WordleDictionary.java

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,9 +1,27 @@
 package ru.yandex.practicum;
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
     ему нужны методы по загрузке списка слов из файла по имени файла
     на выходе должен быть класс WordleDictionary
  */
 public class WordleDictionaryLoader {
+    public WordleDictionary loadDictionary(String filePath) throws IOException {
+        WordleDictionary dictionary = new WordleDictionary();
+        try {
+            File file = new File(filePath);
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    String word = scanner.nextLine();
+                    dictionary.addWord(word);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка чтения файла: " + e.getMessage());
+        }
+        return dictionary;
+    }
 }
