5eb129bf5626d857917cd24ae1102cc9b45687e2
Olga Sova <sova_277@mail.ru>
2026-03-23 18:20:42 +0400

    Удалён логгер

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,24 +1,10 @@
 package ru.yandex.practicum;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 
-/*
-в главном классе нам нужно:
-    создать лог-файл (он должен передаваться во все классы)
-    создать загрузчик словарей WordleDictionaryLoader
-    загрузить словарь WordleDictionary с помощью класса WordleDictionaryLoader
-    затем создать игру WordleGame и передать ей словарь
-    вызвать игровой метод в котором в цикле опрашивать пользователя и передавать информацию в игру
-    вывести состояние игры и конечный результат
- */
 public class Wordle {
-    private static final Logger logger = LoggerFactory.getLogger(Wordle.class);
-
     public static void main() {
-        logger.info("Начата загрузка словаря");
+        System.out.println("Начата загрузка словаря");
         WordleDictionaryLoader loader = new WordleDictionaryLoader();
         WordleDictionary dictionary;
         try {
@@ -26,9 +12,10 @@ public class Wordle {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        logger.info("Словарь загружен успешно");
+        System.out.println("Словарь загружен успешно");
 
         WordleGame game = new WordleGame(dictionary);
         game.startGame();
     }
 }
+
