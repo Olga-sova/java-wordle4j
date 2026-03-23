d824bd356f0a6f98bbe6bf3d56dce166cd760346
Olga Sova <sova_277@mail.ru>
2026-03-23 18:10:13 +0400

    Удалён файл pom.xml

--- src/ru/yandex/practicum/WordNotFoundException.java
+++ src/ru/yandex/practicum/WordNotFoundException.java
@@ -1,6 +1,11 @@
 package ru.yandex.practicum;
 
+import java.io.Serial;
+
 public class WordNotFoundException extends Exception {
+    @Serial
+    private static final long serialVersionUID = 513302186335061222L;
+
     public WordNotFoundException(String message) {
         super(message);
     }
